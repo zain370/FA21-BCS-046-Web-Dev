2663b790c392ee7d7dd7b3e8481d7f200dc43c4a
Zain <zainy370@gmail.com>
2024-03-16 12:43:50 +0500

    removed extra code

--- Assignment 2/Javascript/custom.js
+++ Assignment 2/Javascript/custom.js
@@ -1,15 +1,19 @@
-$("#my-form").validate({
+$("#myform").validate({
 
     rules: {
         name: {
-            minlength: 2
+            minlength: 3
+        },
+        email:{
+            email:true
         }
     },
     messages: {
         name: {
             required: "Please enter your name",
-            minlength: "Name atleast 2 character"
-        }
+            minlength: "Name atleast 3 character"
+        },
+        email:"Please enter your email"
     },
 
     submitHandler: function (form) {
