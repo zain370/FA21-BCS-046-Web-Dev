8b73c891fda01812dd728c3d94b1c4f5c5972905
Zain <zainy370@gmail.com>
2024-03-17 15:36:35 +0500

    form validation compelted

--- Assignment 2/Javascript/custom.js
+++ Assignment 2/Javascript/custom.js
@@ -1,22 +1,36 @@
 $("#myform").validate({
-
     rules: {
         name: {
-            minlength: 3
+            minlength: 3,
+            lettersOnly: true // Custom validation method for allowing only alphabetical characters
         },
-        email:{
-            email:true
+        email: {
+            email: true
         }
     },
     messages: {
         name: {
             required: "Please enter your name",
-            minlength: "Name atleast 3 character"
+            minlength: "Name should be at least 3 characters",
+            lettersOnly: "Please enter only alphabetical characters"
         },
-        email:"Please enter your email"
+        email: "Please enter your email"
     },
-
-    submitHandler: function (form) {
-        form.submit();
+    submitHandler: function (mailForm) {
+        $.ajax({
+            url: mailForm.action,
+            type: mailForm.method,
+            data: $(mailForm).serialize(),
+            success: function (response) {
+                $("#message-display").html("Thanks! Your message has been sent.");
+                $("#submit-btn-wrapper").hide(); // Hide the submit button wrapper
+                $(mailForm)[0].reset();
+            }            
+        });
     }
 });
+
+// Custom validation method for allowing only alphabetical characters
+$.validator.addMethod("lettersOnly", function (value, element) {
+    return this.optional(element) || /^[A-Za-z]+$/.test(value);
+}, "Please enter only alphabetical characters.");
